e3d13f0121ebac1d5a41592a1f706ad1345448bd
roger-mahler <roger.mahler@hotmail.com>
2020-05-28 12:54:27 +0200

    Changes settings

--- pypclda/__init__.py
+++ pypclda/__init__.py
@@ -1,9 +1,15 @@
 
-from .pypclda import get_util, get_logging_utils, \
+from .pypclda import get_util, create_logging_utils, \
     create_lda_sampler_of_type_with_factory, \
-    create_lda_sampler_of_type, \
-    create_simple_lda_config, \
-    load_lda_dataset, \
-    load_lda_sampler, \
-    create_lda_dataset, \
-    sample_pclda
+    create_lda_sampler_of_type, create_simple_lda_config, \
+    load_lda_dataset, load_lda_sampler, \
+    create_lda_dataset, sample_pclda, get_alphabet, \
+    extract_vocabulary, extract_token_counts, extract_doc_lengths, \
+    get_token_topic_matrix, get_document_topic_matrix, \
+    get_topic_token_phi_matrix, \
+    get_top_topic_tokens, \
+    get_top_topic_word_relevances, \
+    calculate_token_probs, \
+    calculate_token_probs_given_topic, \
+    calculate_token_relevance_matrix
+
